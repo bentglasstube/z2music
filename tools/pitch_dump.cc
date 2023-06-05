a34261359ca788244d6a108f53151f91deef1199
Alan Berndt <alan@eatabrick.org>
2023-06-05 10:17:36 -0500

    Slight improvements to pitch dump tool.

--- tools/pitch_dump.cc
+++ tools/pitch_dump.cc
@@ -1,20 +1,30 @@
 #include <iostream>
+#include <sstream>
 
+#include "absl/flags/parse.h"
+#include "absl/flags/usage.h"
 #include "pitch.h"
 #include "rom.h"
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <rom>" << std::endl;
-    return 1;
-  }
+  std::ostringstream usage;
+  usage << "Dumps the pitch LUT from the given rom." << std::endl;
+  usage << "Example usage:" << std::endl;
+  usage << argv[0] << " <rom>";
+  absl::SetProgramUsageMessage(usage.str());
 
-  z2music::Rom rom(argv[1]);
+  auto args = absl::ParseCommandLine(argc, argv);
+  z2music::Rom rom(args[1]);
   auto lut = rom.pitch_lut();
 
+  std::cout << std::hex << "  ";
+  for (size_t i = 0; i < 16; i += 2) {
+    std::cout << "    " << std::setfill('0') << std::setw(2) << i;
+  }
+  std::cout << std::endl;
   for (size_t i = 0x00; i < 0x7a; i += 2) {
     if (i % 0x10 == 0) {
-      std::cout << std::hex << std::setfill('0') << std::setw(2) << i;
+      std::cout << std::setfill('0') << std::setw(2) << i;
     }
 
     std::cout << std::setfill(' ') << std::setw(6) << lut->at(i).to_string();
