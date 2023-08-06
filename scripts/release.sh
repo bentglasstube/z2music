0f991df5161e414b82a3daf9e7e043ba7c3457da
Alan Berndt <alan@eatabrick.org>
2023-08-06 11:01:27 -0700

    Add dlls to release zip.

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,9 @@ set -e
 
 make_release() {
   local version=$(git describe --tags --long)
+
+  echo "Building release for version $version"
+
   bazel test :all
   bazel build -c opt tools:modder_windows tools:music_dump_windows
 
@@ -11,6 +14,7 @@ make_release() {
   cp bazel-bin/tools/modder.exe z2music
   cp bazel-bin/tools/music_dump.exe z2music
   cp projects/amida.z2music z2music
+  cp bazel-z2music/external/gcc_mxe_mingw64_files/x86_64-w64-mingw32.shared/bin/*.dll z2music
 
   local zipfile="z2music-$version.zip"
   zip "$zipfile" z2music/*
