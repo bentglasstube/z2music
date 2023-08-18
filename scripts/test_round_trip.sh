1a12254d6808ab5ce23d17a550f1486f21ab4466
Alan Berndt <alan@eatabrick.org>
2023-08-18 16:01:28 -0700

    Update test script

--- scripts/test_round_trip.sh
+++ scripts/test_round_trip.sh
@@ -5,9 +5,16 @@ set -e
 main() {
   local rom="$1"
 
-  local project_file="$(mktemp)"
-  local rom_file="$(mktemp)"
-  local second_file="$(mktemp)"
+  if [ -z "$rom" ]; then
+    echo "Usage: $0 <romfile>" >&2
+    exit 1
+  fi
+
+  local name=$(basename "$rom" .nes)
+
+  local project_file="$(mktemp "$name-XXXXXX-first.z2music")"
+  local rom_file="$(mktemp "$name-XXXXXX.nes")"
+  local second_file="$(mktemp "$name-XXXXXX-second.z2music")"
 
   bazel build -c opt tools:modder tools:music_dump
   bazel-bin/tools/music_dump --minloglevel 1 --rom "$rom" > "$project_file"
@@ -15,6 +22,8 @@ main() {
   bazel-bin/tools/music_dump --minloglevel 1 --rom "$rom_file" > "$second_file"
 
   vimdiff "$project_file" "$second_file"
+
+  rm -f "$project_file" "$rom_file" "$second_file"
 }
 
 main "$@"
