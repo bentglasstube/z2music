c3f6e7c2f0d23d819d05a98f29af524261e195b2
Alan Berndt <alan@eatabrick.org>
2023-08-18 00:10:49 -0700

    Improve modder test script

--- scripts/test_modder.sh
+++ scripts/test_modder.sh
@@ -9,8 +9,13 @@ function test() {
   local input="$1"
   local output="$(mktemp)"
 
+  if [ -z "$input" ]; then
+    echo "Usage: $0 <romfile>" >&2
+    exit 1
+  fi
+
   bazel run -c opt tools:modder -- $(pwd)/projects/$PROJECT.z2music --rom "$1" --output "$output" --minloglevel 1
-  retroarch -L /usr/lib/libretro/${CORE}_libretro.so "$output"
+  fceux "$output"
 }
 
 test "$@"
